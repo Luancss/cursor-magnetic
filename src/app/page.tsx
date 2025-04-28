bb8c84a3ec6308b3e6345e3c243861fbacaf11cb
Luan <ventobo123@gmail.com>
2025-04-27 22:36:52 -0300

    feat: Add useEffect to manage grid item highlights and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement>(null);
   const highlightRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const highlight = highlightRef.current;
+    const gridItems = container?.querySelectorAll(".grid-item");
+    const firstItem = gridItems?.[0];
+
+    const hightlightColors = [
+      "#E24E1B",
+      "#4381C1",
+      "#F79824",
+      "#04A777",
+      "#5BBC5A",
+      "#21F6FF",
+      "#818D92",
+      "#22AAA1",
+    ];
+  });
 
   return (
     <>
